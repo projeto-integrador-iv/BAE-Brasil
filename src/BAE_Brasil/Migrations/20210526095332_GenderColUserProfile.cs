3d1f661d99d23c4acb0ccbd864fe821fcdc748cb
Gustavo Coelho Belleza Dias <gustavo.coelho@dtidigital.com.br>
2021-05-26 08:13:02 -0300

    Adicionadas migrations

---
+++ src/BAE_Brasil/Migrations/20210526095332_GenderColUserProfile.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BAE_Brasil.Migrations
+{
+    public partial class GenderColUserProfile : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Gender",
+                table: "Profiles",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Gender",
+                table: "Profiles");
+        }
+    }
+}
